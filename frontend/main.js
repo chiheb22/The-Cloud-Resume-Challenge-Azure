911422b11883585bbbb84164b8004aba52a8fe0c
Chiheb KHEMIRI <chiheb.khemiri@piterion.com>
2024-07-11 10:32:01 +0100

    add counter functionalty

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+  });
+  
+const functionApi = '';
+  
+  const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi)
+      .then(response => {
+        return response.json()
+      })
+      .then(response => {
+        count = response;
+        console.log("Hello 👋, you are visitor number - " + count);
+        document.getElementById('counter').innerText = count;
+      }).catch(function (error) {
+        console.log(error);
+      });
+    return count;
+  }
